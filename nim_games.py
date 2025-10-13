c10954dc2331184e0e3e1a764208a075c84250dd
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-13 13:18:55 +0200

    Choix du type de partie

--- nim_games.py
+++ nim_games.py
@@ -4,14 +4,27 @@
 """
 Jeu de Nim (variante simple et de Marienbad)
 """
-import random  # Importation correcte du module
+import random
 
-nameplayer1 = input("Entrer le nom du joueur 1: ")
-nameplayer2 = input("Entrer le nom du joueur 2: ")
+while True:
+    typeofgame = input("Tapez ORDINATEUR ou JOUEUR : ")
 
-random = random.randint(1, 2)
+    if typeofgame == "JOUEUR":
+        nameplayer1 = input("Entrez le nom du joueur 1 : ")
+        nameplayer2 = input("Entrez le nom du joueur 2 : ")
 
-if random == 1:
-    print(f"{nameplayer1} commence la partie.")
-else:
-    print(f"{nameplayer2} commence la partie.")
+        starter = random.randint(1, 2)
+
+        if starter == 1:
+            print(f"{nameplayer1} commence la partie.")
+        else:
+            print(f"{nameplayer2} commence la partie.")
+        break
+
+    elif typeofgame == "ORDINATEUR":
+        nameplayer1 = input("Entrez le nom du joueur : ")
+        print(f"{nameplayer1} va jouer contre l'ordinateur.")
+        break
+
+    else:
+        print("Entrée invalide. Veuillez taper ORDINATEUR ou JOUEUR.")
